8be58d97d7ecc30afac55b1635e7120471be118f
Roger Welin <roger.welin@icloud.com>
2019-08-05 17:01:19 +0200

    more broken

--- src/consul/client/kv.cr
+++ src/consul/client/kv.cr
@@ -1,10 +1,12 @@
 require "base64"
 
-class KeyValue
-  JSON.mapping(
-    address: String,
-    location: {type: Location, nilable: true},
-  )
+module Types
+  class KV
+    JSON.mapping(
+      address: String,
+      location: {type: Location, nilable: true},
+    )
+  end
 end
 
 module Consul
@@ -19,7 +21,7 @@ module Consul
 
       def get_key : KV
         resp = HTTP::Client.get("http://localhost:8500/v1/kv/apa")
-        kv = Array(KeyValue).from_json(resp.body)
+        kv = Array(Types::KV).from_json(resp.body)
         keyval = KV.new(kv.first.key, Base64.decode_string(kv.first.value))
         return keyval
       end
