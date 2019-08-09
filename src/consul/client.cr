ccde3a061bde48b8293ad2acb8fbd2f8abce155f
Roger Welin <roger.welin@icloud.com>
2019-08-09 15:52:57 +0200

    add token/auth support

--- src/consul/client.cr
+++ src/consul/client.cr
@@ -17,7 +17,7 @@ module Consul
       @token   : String? = nil    # to be implemented
       )
 
-      client = http_client_instance("#{scheme}://#{host}:#{port}")
+      client = http_client_instance("#{scheme}://#{host}:#{port}", token)
 
       @kv      = Consul::KV.new(client)
       @catalog = Consul::Catalog.new(client)
@@ -27,10 +27,17 @@ module Consul
       @health  = Consul::Health.new(client)
     end
 
-    private def http_client_instance(uri : String) : HTTP::Client
+    private def http_client_instance(uri : String, token : String? = nil) : HTTP::Client
       uri = URI.parse(uri)
       client = HTTP::Client.new(uri)
       client.connect_timeout = 5
+
+      if token
+        client.before_request do |request|
+          request.headers["X-Consul-Token"] = token
+        end
+      end
+
       return client
     end
 
