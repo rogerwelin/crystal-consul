ff07da8a1de8613a961f6ec5acfaea383a63f788
Roger Welin <roger.welin@icloud.com>
2019-08-14 14:19:52 +0200

    fix indentation

--- spec/status_spec.cr
+++ spec/status_spec.cr
@@ -1,18 +1,18 @@
 require "./spec_helper"
 
 describe Consul do
-    context "status" do
+  context "status" do
   
-      it "should return a string for leader endpoint" do
-        c = Consul.client()
-        c.status.get_leader().should be_a String
-      end
-
-      it "should return a array of strings for raft peers" do
-        c = Consul.client()
-        c.status.list_raft_peers().should be_a Array(String)
-      end
-  
+    it "should return a string for leader endpoint" do
+      c = Consul.client()
+      c.status.get_leader().should be_a String
     end
+
+    it "should return a array of strings for raft peers" do
+      c = Consul.client()
+      c.status.list_raft_peers().should be_a Array(String)
+    end
+  
   end
+end
   
